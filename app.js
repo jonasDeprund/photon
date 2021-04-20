ed72dcb3ecc39480476f70cd6e63af7cd2f42232
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-20 18:51:29 +0200

    fetch data

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const auth = '563492ad6f91700001000001ee39bf7723944ecd87c8df3505321ff3'
 const gallery = document.querySelector('.gallery')
-const searchInput = document.querySelector('search-input')
-const submitButton = document.querySelector('.submit-btn')
+const searchInput = document.querySelector('.search-input')
+const form = document.querySelector('.search-form')
 let searchValue
+
+// Event listeners
+searchInput.addEventListener('input', updateInput)
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  searchPhotos(searchValue)
+})
+
+function updateInput(e) {
+  searchValue = e.target.value
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
-    'https://api.pexels.com/v1/curated?per_page=1',
+    'https://api.pexels.com/v1/curated?per_page=15&page=1',
     {
       method: 'GET',
       headers: {
@@ -17,11 +28,31 @@ async function curatedPhotos() {
   )
   const data = await dataFetch.json()
   data.photos.forEach((photo) => {
-    console.log(photo)
     const galleryImg = document.createElement('div')
     galleryImg.classList.add('gallery-img')
     galleryImg.innerHTML = `<img src="${photo.src.large}"></img>
     <p>${photo.photographer}</p>`
+    gallery.appendChild(galleryImg)
+  })
+}
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: auth,
+      },
+    }
+  )
+  const data = await dataFetch.json()
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement('div')
+    galleryImg.classList.add('gallery-img')
+    galleryImg.innerHTML = `<img src="${photo.src.large}"></img>
+        <p>${photo.photographer}</p>`
     gallery.appendChild(galleryImg)
   })
 }

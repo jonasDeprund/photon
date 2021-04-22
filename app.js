d89c3e4f4b0088a24e6edcf87dcb05ca1c5384f6
Jonas Deprund <jonasdeprund@gmail.com>
2021-04-22 16:27:58 +0200

    refactoring

--- app.js
+++ app.js
@@ -15,39 +15,19 @@ function updateInput(e) {
   searchValue = e.target.value
 }
 
-async function curatedPhotos() {
-  const dataFetch = await fetch(
-    'https://api.pexels.com/v1/curated?per_page=15&page=1',
-    {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: auth,
-      },
-    }
-  )
-  const data = await dataFetch.json()
-  data.photos.forEach((photo) => {
-    const galleryImg = document.createElement('div')
-    galleryImg.classList.add('gallery-img')
-    galleryImg.innerHTML = `<img src="${photo.src.large}"></img>
-    <p>${photo.photographer}</p>`
-    gallery.appendChild(galleryImg)
+async function fetchApi(url) {
+  const dataFetch = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: auth,
+    },
   })
+  const data = await dataFetch.json()
+  return data
 }
 
-async function searchPhotos(query) {
-  const dataFetch = await fetch(
-    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
-    {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        Authorization: auth,
-      },
-    }
-  )
-  const data = await dataFetch.json()
+function generatePictures(data) {
   data.photos.forEach((photo) => {
     const galleryImg = document.createElement('div')
     galleryImg.classList.add('gallery-img')
@@ -55,6 +35,20 @@ async function searchPhotos(query) {
         <p>${photo.photographer}</p>`
     gallery.appendChild(galleryImg)
   })
+}
+
+async function curatedPhotos() {
+  const data = await fetchApi(
+    'https://api.pexels.com/v1/curated?per_page=15&page=1'
+  )
+  generatePictures(data)
+}
+
+async function searchPhotos(query) {
+  const data = await fetchApi(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+  )
+  generatePictures(data)
 }
 
 curatedPhotos()
